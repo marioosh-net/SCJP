71d127d8ca0911c0ed99952e3f0c8a9b312651f5
muniek <muniek@b03a516d-aff6-4282-84b8-a76f129381e4>
2009-10-06 13:33:08 +0000

    git-svn-id: svn+ssh://marioosh.net/var/svn/SCJP@22 b03a516d-aff6-4282-84b8-a76f129381e4

--- eclipse/src/ch6/Format.java
+++ eclipse/src/ch6/Format.java
@@ -1,16 +1,31 @@
 package ch6;
 
 /**
+ * format = printf
  * 
  * format string:
  * %[arg_index$][flags][width][.precision]conversionChar
- * gdzie [] - opcjonalne;
+ * ([] - opcjonalne)
+ * 
+ * gdzie:
+ * 
  * conversionChar:
  * - b boolean
  * - c char
  * - d integer
  * - f floating point
  * - s string
+ * 
+ * flags:
+ * - "-" Left justify this argument
+ * - "+" Include a sign (+ or -) with this argument
+ * - "0" Pad this argument with zeroes
+ * - "," Use locale-specific grouping separators (i.e., the comma in 123,456)
+ * - "(" Enclose negative numbers in parentheses
+ * 
+ * width - minimalna ilosc znakow do wydrukowania
+ * 
+ * precission - ilosc drukowanych miejsc po przecinku (dla zmiennoprzecinkowych)
  * 
  */
 public class Format {
@@ -20,10 +35,23 @@ public class Format {
 
 	void examples() {
 
-		System.out.printf("%2$d %1$d", 10, 20);
+		System.out.printf("%2$d %1$d \n", 10, 20);
 
 		// java.util.MissingFormatArgumentException
-		// nie istnieje argument o numerze 2
-		System.out.printf("%2$d", 10);
+		// nie istnieje argument o numerze 2 
+		/*
+		System.out.printf("%2$d", 10); */
+
+		int i1 = -123;
+		int i2 = 12345;
+		System.out.printf(">%1$(7d< \n", i1);
+		System.out.printf(">%0,7d< \n", i2);
+		System.out.format(">%+-7d< \n", i2);
+		System.out.printf(">%2$b + %1$5d< \n", i1, false);
+		
+		/* java.util.IllegalFormatConversionException
+		System.out.format("%d", 12.37); */
+		
+		System.out.format("%.1f", 12.35); // zaokragla w gore /-> 12.4
 	}
 }
