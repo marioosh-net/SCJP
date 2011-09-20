a4c6e855c4823e8e3389a03450d0939753f1b0c2
marioosh <marioosh.net@gmail.com>
2011-09-20 17:03:19 +0200

    threads...

--- eclipse/src/ch9/MyThread.java
+++ eclipse/src/ch9/MyThread.java
@@ -3,6 +3,7 @@ package ch9;
 public class MyThread extends Thread {
 	public void run() {
 		System.out.println("Important job running in MyThread");
+		System.out.println("NAME: "+getName());
 	}
 
 	// to jest ignorowane, bo to jest przeladowanie metody run()
--- eclipse/src/ch9/MyThreadByRunnable.java
+++ eclipse/src/ch9/MyThreadByRunnable.java
@@ -5,6 +5,7 @@ public class MyThreadByRunnable implements Runnable {
 	@Override
 	public void run() {
 		System.out.println("Important job running in MyThreadByRunnable");
+		System.out.println("NAME: "+Thread.currentThread().getName());
 	}
 
 }
