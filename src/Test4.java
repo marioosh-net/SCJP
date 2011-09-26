804517b3aaed0989d6de70f5182fcddea0825019
marioosh <marioosh.net@gmail.com>
2011-09-26 22:36:42 +0200

    equals

---
+++ src/Test4.java
@@ -0,0 +1,12 @@
+
+public class Test4 {
+	public static void main(String[] args) {
+		StringBuffer b = new StringBuffer("1");
+		StringBuffer b2 = new StringBuffer("1");
+		Integer i = new Integer("1");
+		Integer i2 = new Integer("1");
+		System.out.println(i2.equals(i));
+		System.out.println(b2.equals(b));
+		System.out.println(12 | 2);
+	}
+}
